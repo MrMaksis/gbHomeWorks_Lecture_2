0db4dfc614a693f9e1b30051dfaa3c2af2faaed4
mr_maksis <max.morink@yandex.ru>
2022-12-20 14:10:52 +0300

    Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task_1
+
+int SearchDivisible(int num1)
+{
+    int sot = num1 / 10;
+    int md = sot % 10;
+    return md;
+}
+
+Console.Write("Введите трехзначное число: ");
+Console.WriteLine($"Второе число: {SearchDivisible(Convert.ToInt32(Console.ReadLine()))}");
