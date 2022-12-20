791f62203fd3f21afe11e156c20db42ec14dc5d1
mr_maksis <max.morink@yandex.ru>
2022-12-20 14:38:21 +0300

    Task 3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task_3
+
+bool IsEntrance(int day)
+{
+    if(day > 5 || day == 7){
+        return true;
+    }
+    else    
+        return false;
+}
+
+Console.Write("Введите день недели: ");
+bool en = IsEntrance(Convert.ToInt32(Console.ReadLine()));
+
+if (en == true)
+    Console.WriteLine("Сегодня выходной!");
+else
+    Console.WriteLine("Мы еще работаем!");
